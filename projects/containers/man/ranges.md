070d84e83317689d8a6791d950482967a7de80e1
dainank <whelbe@outlook.com>
2023-05-29 09:16:09 +0200

    ranges cpp

---
+++ projects/containers/man/ranges.md
@@ -0,0 +1,1 @@
+# Using Ranges to Display Vectors
---
+++ projects/containers/src/ranges.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> get_next_row(const std::vector<int> &previous_row)
+{
+    std::vector next_row{1}; // always starts with 1
+    if (previous_row.empty())
+    {
+        return next_row;
+    }
+    for (size_t idx = 0; idx + 1 < previous_row.size(); ++idx)  // unsigned (0+) integer type scales to compiler
+    {
+        next_row.emplace_back(previous_row[idx] + previous_row[idx + 1]);
+    }
+    next_row.emplace_back(1); // always ends with 1
+    return next_row;
+}
+
+auto generate_triangle(int rows) // auto can be used to declare variables with complicated types
+{
+    std::vector<std::vector<int>> triangle{{1}}; // top generic row
+    for (int row = 1; row < rows; ++row)
+    {
+        triangle.push_back(get_next_row(triangle.back()));
+    }
+    return triangle;
+}
+
+void print_triangle(std::vector<int> &row)
+{
+    for (auto number : row)
+    {
+        std::cout << number << " ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &s,
+                         const std::vector<std::vector<T>> &triangle)
+{
+    for (const auto &row : triangle) // const ref avoids copy
+    {
+        std::ranges::copy(row, std::ostream_iterator<T>(s, " "));
+        s << '\n';
+    }
+    return s;
+}
+
+int main()
+{
+    auto triangle = generate_triangle(3);
+    std::cout << triangle;
+}
